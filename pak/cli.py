54be82ab5cf030f40d3ac2cf55259077005261aa
Tyler Kennedy <tk@tkte.ch>
2017-09-20 05:33:27 -0400

    PEP8

--- pak/cli.py
+++ pak/cli.py
@@ -10,7 +10,7 @@ from pak.reader import PAKFileReader
 
 
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
